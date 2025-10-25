7d161c2c0386ff95b9032294e9c97aacbb5ac49e
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-10-26 02:18:43 +0800

    Update available_pics.js

--- fotorama/EP42C9/available_pics.js
+++ fotorama/EP42C9/available_pics.js
@@ -1,14 +1,82 @@
 let available_pics = [
     {
-		picture: "./Pictures/woman-3584435_960_720.jpg",
-		description: "Red hair woman"
+		picture: "./Pictures/20251018_102617.jpg",
+		description: "1"
 	},
 	{
-		picture: "./Pictures/woman-8086721_960_720.jpg",
-		description: "Asiatic woman"
+		picture: "./Pictures/20251018_113706.jpg",
+		description: "2"
 	},
 	{
-		picture: "./Pictures/woman-1466628_960_720.jpg",
-		description: "Blond woman"
+		picture: "./Pictures/20251018_151822.jpg",
+		description: "3"
+	},
+	{
+		picture: "./Pictures/20251019_081651.jpg",
+		description: "4"
+	},
+	{
+		picture: "./Pictures/20251019_095413.jpg",
+		description: "5"
+	},
+	{
+		picture: "./Pictures/20251019_095542.jpg",
+		description: "6"
+	},
+	{
+		picture: "./Pictures/20251019_095656.jpg",
+		description: "7"
+	},
+	{
+		picture: "./Pictures/20251019_103149.jpg",
+		description: "8"
+	},
+	{
+		picture: "./Pictures/20251019_104136.jpg",
+		description: "9"
+	},
+	{
+		picture: "./Pictures/20251019_134254.jpg",
+		description: "10"
+	},
+	{
+		picture: "./Pictures/20251019_155224.jpg",
+		description: "11"
+	},
+	{
+		picture: "./Pictures/20251020_162028.jpg",
+		description: "12"
+	},
+	{
+		picture: "./Pictures/20251020_162148.jpg",
+		description: "13"
+	},
+	{
+		picture: "./Pictures/20251020_170454.jpg",
+		description: "14"
+	},
+	{
+		picture: "./Pictures/20251021_123106.jpg",
+		description: "15"
+	},
+	{
+		picture: "./Pictures/20251021_135726.jpg",
+		description: "16"
+	},
+	{
+		picture: "./Pictures/20251021_135802.jpg",
+		description: "17"
+	},
+	{
+		picture: "./Pictures/20251021_141648.jpg",
+		description: "18"
+	},
+	{
+		picture: "./Pictures/20251021_142852.jpg",
+		description: "19"
+	},
+	{
+		picture: "./Pictures/IMG-20251021-WA0001.jpg",
+		description: "20"
 	}
 ];
