96a5e7ea2a4600b35f73fb6e4b202ad998f86691
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-12-20 23:02:10 +0800

    Creating new fotorama

---
+++ fotorama/EP42C10/available_pics.js
@@ -0,0 +1,74 @@
+let available_pics = [
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.44(1).jpeg",
+    description:"1"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.44(2).jpeg",
+    description:"2"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.44.jpeg",
+    description:"3"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.45(1).jpeg",
+    description:"4"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.45(2).jpeg",
+    description:"5"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.45(3).jpeg",
+    description:"6"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.45(4).jpeg",
+    description:"7"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.45.jpeg",
+    description:"8"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.46(1).jpeg",
+    description:"9"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.46(2).jpeg",
+    description:"10"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.46(3).jpeg",
+    description:"11"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.46.jpeg",
+    description:"12"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.47(1).jpeg",
+    description:"13"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.47(2).jpeg",
+    description:"14"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.47(3).jpeg",
+    description:"15"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.47.jpeg",
+    description:"16"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.48(1).jpeg",
+    description:"17"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-20 at 11.52.48.jpeg",
+    description:"18"
+  }
+];
