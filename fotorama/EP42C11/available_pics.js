9a5f05d2dd61f2c03086d826326d5f6ff5a514f3
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2025-12-25 06:26:38 +0800

    Fotorama EP42C11

---
+++ fotorama/EP42C11/available_pics.js
@@ -0,0 +1,102 @@
+let available_pics = [
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.34.jpeg",
+    description:"1"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.35(1).jpeg",
+    description:"2"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.35.jpeg",
+    description:"3"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.36(1).jpeg",
+    description:"4"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.36(2).jpeg",
+    description:"5"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.36.jpeg",
+    description:"6"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.37(1).jpeg",
+    description:"7"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.37(2).jpeg",
+    description:"8"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.37.jpeg",
+    description:"9"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.38(1).jpeg",
+    description:"10"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.38.jpeg",
+    description:"11"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.39(1).jpeg",
+    description:"12"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.39(2).jpeg",
+    description:"13"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.39(3).jpeg",
+    description:"14"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.39.jpeg",
+    description:"15"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.40(1).jpeg",
+    description:"16"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.40(2).jpeg",
+    description:"17"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.40.jpeg",
+    description:"18"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.41(1).jpeg",
+    description:"19"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.41(2).jpeg",
+    description:"20"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.41.jpeg",
+    description:"21"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.42(1).jpeg",
+    description:"22"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.42(2).jpeg",
+    description:"23"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.42(3).jpeg",
+    description:"24"
+  },
+  {
+    picture:"./Pictures/WhatsApp Image 2025-12-24 at 18.30.42.jpeg",
+    description:"25"
+  }
+];
