338fdf4fd14955992665c87b390dffaeff64e71c
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2026-03-06 14:40:05 -0300

    Create config.js

---
+++ farewell/config.js
@@ -0,0 +1,18 @@
+localStorage.setItem("song", "Europe_The_Final_Countdown_Edited.mp3")
+localStorage.setItem("image", "sad_raccoon.jpg")
+localStorage.setItem("text", 
+    `
+    This message will touch your heart
+    making you cry a lot.
+    I do not want to say good bye,
+    but now is the time.
+    Things not happens as we planned
+    and I never wanted to say goodbye,
+    so farewell.
+    I have many things in my mind,
+    I have many things in my heart,
+    many words never said,
+    many farewells never end. 
+    Thanks!
+    `
+)
