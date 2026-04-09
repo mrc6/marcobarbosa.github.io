e02630e3410a532d293b581ca64fcbac3e6ef4fe
Marco Barbosa <60627858+mrc6@users.noreply.github.com>
2026-04-09 16:50:05 -0300

    Create config.js

---
+++ pro_home_page/config.js
@@ -0,0 +1,18 @@
+localStorage.setItem("song", "Europe_The_Final_Countdown_Edited.mp3")
+localStorage.setItem("image", "2601_me.jpg")
+localStorage.setItem("delayed_image", "2601_me_laser_eyes.png")
+localStorage.setItem("text", 
+    `
+    I have many years of solid experience with Automated Tests.
+    I am passionate about programming and quality.
+    Currently I work as Product Validation Engineer using Python, Bash, GitHub Actions and Other tools.
+    Previously I worked as Automated Test Analyst with Java + Selenium Web Driver 
+    and Java + Rest Assured in addition to other technologies.
+    My motivation is to grow in my career and develop myself as a member of a team.
+    Thank you very much to have time to know me better.
+    I will wait your contact.
+    Regards,
+    Marco.
+    :)
+    `
+)
